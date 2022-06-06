398ed494fa35be4f19308f6a61b7576bad776bd1
Federico Balderas <federico.balderas@zenki.fi>
2022-06-06 12:06:04 -0500

    ZEN-1361 Se envían nuevas propiedades a la orden de Zenkipay (shopperCarId y merchantOrderId)

--- Model/Zenkipay.php
+++ Model/Zenkipay.php
@@ -27,10 +27,8 @@ use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 
-
 class Zenkipay extends \Magento\Payment\Model\Method\AbstractMethod
 {
-
     const CODE = 'zenki_zenkipay';
 
     protected $_code = self::CODE;
@@ -41,66 +39,58 @@ class Zenkipay extends \Magento\Payment\Model\Method\AbstractMethod
     protected $_canRefund = false;
     protected $_canRefundInvoicePartial = false;
     protected $_canAuthorize = true;
-    protected $_canVoid = true;    
-    protected $is_sandbox;           
-    protected $sk;    
-    protected $sandbox_sk;    
-    protected $live_sk;        
-    protected $scopeConfig;              
+    protected $_canVoid = true;
+    protected $is_sandbox;
+    protected $pk;
+    protected $sandbox_pk;
+    protected $live_pk;
+    protected $base_url;
+    protected $scopeConfig;
     protected $logger;
-    protected $_storeManager;                
-    protected $customerSession;        
-    protected $configWriter;    
+    protected $_storeManager;
+    protected $customerSession;
+    protected $configWriter;
 
-   /**    
-    *
-    * @param StoreManagerInterface $storeManager
-    * @param Context $context
-    * @param Registry $registry
-    * @param ExtensionAttributesFactory $extensionFactory
-    * @param AttributeValueFactory $customAttributeFactory
-    * @param Data $paymentData
-    * @param ScopeConfigInterface $scopeConfig
-    * @param WriterInterface $configWriter
-    * @param Logger $logger
-    * @param ModuleListInterface $moduleList
-    * @param TimezoneInterface $localeDate
-    * @param CountryFactory $countryFactory
-    * @param LoggerInterface $logger_interface    
-    * @param CustomerSession $customerSession
-    * @param array $data
-    */
+    /**
+     *
+     * @param StoreManagerInterface $storeManager
+     * @param Context $context
+     * @param Registry $registry
+     * @param ExtensionAttributesFactory $extensionFactory
+     * @param AttributeValueFactory $customAttributeFactory
+     * @param Data $paymentData
+     * @param ScopeConfigInterface $scopeConfig
+     * @param WriterInterface $configWriter
+     * @param Logger $logger
+     * @param ModuleListInterface $moduleList
+     * @param TimezoneInterface $localeDate
+     * @param CountryFactory $countryFactory
+     * @param LoggerInterface $logger_interface
+     * @param CustomerSession $customerSession
+     * @param array $data
+     */
     public function __construct(
-            StoreManagerInterface $storeManager,
-            Context $context, 
-            Registry $registry, 
-            ExtensionAttributesFactory $extensionFactory, 
-            AttributeValueFactory $customAttributeFactory, 
-            Data $paymentData, 
-            ScopeConfigInterface $scopeConfig,
-            WriterInterface $configWriter,
-            Logger $logger,            
-            CountryFactory $countryFactory,             
-            LoggerInterface $logger_interface,                        
-            CustomerSession $customerSession,                                               
-            array $data = array()            
+        StoreManagerInterface $storeManager,
+        Context $context,
+        Registry $registry,
+        ExtensionAttributesFactory $extensionFactory,
+        AttributeValueFactory $customAttributeFactory,
+        Data $paymentData,
+        ScopeConfigInterface $scopeConfig,
+        WriterInterface $configWriter,
+        Logger $logger,
+        CountryFactory $countryFactory,
+        LoggerInterface $logger_interface,
+        CustomerSession $customerSession,
+        array $data = []
     ) {
-        
-        parent::__construct(
-            $context,
-            $registry, 
-            $extensionFactory,
-            $customAttributeFactory,
-            $paymentData, 
-            $scopeConfig,
-            $logger,
-            null,
-            null,            
-            $data            
-        );
-                        
-        $this->customerSession = $customerSession;        
-        
+        parent::__construct($context, $registry, $extensionFactory, $customAttributeFactory, $paymentData, $scopeConfig, $logger, null, null, $data);
+
+        $sandbox_url = 'https://dev-gateway.zenki.fi';
+        $url = 'https://uat-gateway.zenki.fi';
+
+        $this->customerSession = $customerSession;
+
         $this->_storeManager = $storeManager;
         $this->logger = $logger_interface;
 
@@ -109,11 +99,12 @@ class Zenkipay extends \Magento\Payment\Model\Method\AbstractMethod
         $this->country_factory = $countryFactory;
 
         $this->is_active = $this->getConfigData('active');
-        $this->is_sandbox = $this->getConfigData('is_sandbox');                                
-        $this->sandbox_pk = $this->getConfigData('sandbox_pk');        
-        $this->live_pk = $this->getConfigData('live_pk');                
-        $this->pk = $this->is_sandbox ? $this->sandbox_pk : $this->live_pk;                                
-    }    
+        $this->is_sandbox = $this->getConfigData('is_sandbox');
+        $this->sandbox_pk = $this->getConfigData('sandbox_pk');
+        $this->live_pk = $this->getConfigData('live_pk');
+        $this->pk = $this->is_sandbox ? $this->sandbox_pk : $this->live_pk;
+        $this->base_url = $this->is_sandbox ? $sandbox_url : $url;
+    }
 
     /**
      * Assign corresponding data
@@ -122,16 +113,15 @@ class Zenkipay extends \Magento\Payment\Model\Method\AbstractMethod
      * @return $this
      * @throws LocalizedException
      */
-    public function assignData(\Magento\Framework\DataObject $data) {
+    public function assignData(\Magento\Framework\DataObject $data)
+    {
         parent::assignData($data);
-                
+
         $infoInstance = $this->getInfoInstance();
-        $additionalData = ($data->getData('additional_data') != null) ? $data->getData('additional_data') : $data->getData();
-        
-        $infoInstance->setAdditionalInformation('zenkipay_order_id', 
-            isset($additionalData['zenkipay_order_id']) ? $additionalData['zenkipay_order_id'] :  null
-        );
-        
+        $additionalData = $data->getData('additional_data') != null ? $data->getData('additional_data') : $data->getData();
+
+        $infoInstance->setAdditionalInformation('zenkipay_order_id', isset($additionalData['zenkipay_order_id']) ? $additionalData['zenkipay_order_id'] : null);
+
         return $this;
     }
 
@@ -143,41 +133,45 @@ class Zenkipay extends \Magento\Payment\Model\Method\AbstractMethod
      * @return $this
      * @throws \Magento\Framework\Exception\LocalizedException
      */
-    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount) {
+    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
         /** @var \Magento\Sales\Model\Order $order */
-        $order = $payment->getOrder();   
-         /** @var \Magento\Sales\Model\Order\Address $billing */
-        $billing = $order->getBillingAddress();
+        $order = $payment->getOrder();
+        /** @var \Magento\Sales\Model\Order\Address $billing */
 
-        $this->logger->debug('#capture', array('$order_id' => $order->getIncrementId(), '$trx_id' => $payment->getLastTransId(), '$status' => $order->getStatus(), '$amount' => $amount));                    
-        
+        $this->logger->debug('#capture', ['$order_id' => $order->getIncrementId(), '$trx_id' => $payment->getLastTransId(), '$status' => $order->getStatus(), '$amount' => $amount]);
+
         if ($amount <= 0) {
             throw new \Magento\Framework\Exception\LocalizedException(__('Invalid amount for capture.'));
-        }               
+        }
 
         $zenkipay_order_id = $this->getInfoInstance()->getAdditionalInformation('zenkipay_order_id');
-                
-        try {                                                             
+
+        try {
             $payment->setAmount($amount);
-            $payment->setTransactionId($zenkipay_order_id);                                            
-            
-            // Registra el ID de la transacción 
-            $order->setExtOrderId($zenkipay_order_id);                        
-            $order->save();              
+            $payment->setTransactionId($zenkipay_order_id);
+
+            // Registra el ID de la transacción
+            $order->setExtOrderId($zenkipay_order_id);
+            $order->save();
+
+            $this->updateZenkipayOrder($zenkipay_order_id, $order->getId());
         } catch (\Exception $e) {
             $this->debugData(['exception' => $e->getMessage()]);
-            $this->_logger->error(__( $e->getMessage()));
+            $this->logger->error(__($e->getMessage()));
             throw new \Magento\Framework\Validator\Exception(__($e->getMessage()));
         }
-        
+
         return $this;
-    }    
-       
-    public function getBaseUrlStore(){
-        return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_LINK);        
     }
-    
-    public function isLoggedIn() {
+
+    public function getBaseUrlStore()
+    {
+        return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_LINK);
+    }
+
+    public function isLoggedIn()
+    {
         return $this->customerSession->isLoggedIn();
     }
 
@@ -187,62 +181,112 @@ class Zenkipay extends \Magento\Payment\Model\Method\AbstractMethod
      * @param \Magento\Quote\Api\Data\CartInterface|null $quote
      * @return bool
      */
-    public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null) {
+    public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null)
+    {
         return parent::isAvailable($quote);
     }
 
-   
-    public function getPublicKey() {
+    public function getPublicKey()
+    {
         return $this->pk;
     }
 
     /**
      * @return boolean
      */
-    public function isSandbox() {
+    public function isSandbox()
+    {
         return $this->is_sandbox;
     }
 
-    public function getCode() {
+    public function getCode()
+    {
         return $this->_code;
     }
 
-    public function validateSettings() {           
-        $response = $this->getMerchantInfo();        
-        $this->logger->debug('#getMerchantInfo', ['response' => $response]);
-        if (!isset($response['access_token'])) {
+    public function validateSettings()
+    {
+        $response = $this->validateZenkipayKey();
+        if (!$response) {
             throw new \Magento\Framework\Validator\Exception(__('Something went wrong while saving this configuration, your Zenkipay key is incorrect.'));
         }
-        
-        return; 
+        return;
     }
-        
-    public function getMerchantInfo() {
-        $url = "https://uat-gateway.zenki.fi/public/v1/merchants/plugin/token";
-       
+
+    protected function validateZenkipayKey()
+    {
+        $result = $this->getAccessToken();
+        if (array_key_exists('access_token', $result)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function getAccessToken()
+    {
+        $url = $this->base_url . '/public/v1/merchants/plugin/token';
+
         $ch = curl_init();
         $payload = $this->pk;
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:text/plain'));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:text/plain']);
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         $result = curl_exec($ch);
-        $response = null;
-        if ($result === false) {
-            $this->logger->error("Curl error", array("curl_errno" => curl_errno($ch), "curl_error" => curl_error($ch)));
-            return $response;
-        } 
-                    
-        $response = json_decode($result, true);                    
-        curl_close($ch);        
-        return $response;
-    }           
 
-    public function getCurrentCurrency() {        
+        if ($result === false) {
+            $this->logger->error('Curl error', ['curl_errno' => curl_errno($ch), 'curl_error' => curl_error($ch)]);
+            return [];
+        }
+
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
+
+    protected function updateZenkipayOrder($zenkipay_order_id, $order_id)
+    {
+        try {
+            $token_result = $this->getAccessToken();
+            if (!array_key_exists('access_token', $token_result)) {
+                $this->logger->error('Zenkipay - updateZenkipayOrder: Error al obtener access_token');
+                return false;
+            }
+
+            $zenkipay_key = $this->pk;
+            $payload = json_encode(['zenkipayKey' => $zenkipay_key, 'merchantOrderId' => $order_id]);
+            $url = $this->base_url . '/v1/orders/' . $zenkipay_order_id;
+            $headers = ['Accept: */*', 'Content-Type: application/json', 'Authorization: Bearer ' . $token_result['access_token']];
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+            $result = curl_exec($ch);
+
+            if ($result === false) {
+                $this->logger->error('Curl error ' . curl_errno($ch) . ': ' . curl_error($ch));
+                return false;
+            }
+
+            curl_close($ch);
+
+            return true;
+        } catch (\Exception $e) {
+            $this->logger->error('Zenkipay - updateZenkipayOrder: ', ['msg' => $e->getMessage(), 'traceAsString' => $e->getTraceAsString()]);
+            return false;
+        }
+    }
+
+    public function getCurrentCurrency()
+    {
         // return $this->_storeManager->getStore()->getCurrentCurrency()->getCode();
         return $this->_storeManager->getStore()->getCurrentCurrencyCode();
     }
-
 }
